8daff6c57d6c67e341f9fadf6743c97cb61a087b
Fernando Flores Quiliche <52175863+fernandobeyond@users.noreply.github.com>
2026-07-01 20:03:30 -0500

    Avance hasta el Sprint 4

--- Assets/Scripts/PropiedadesJugador.cs
+++ Assets/Scripts/PropiedadesJugador.cs
@@ -5,12 +5,16 @@ public class PropiedadesJugador : MonoBehaviour {
     [SerializeField] private float velocidad = 5.5f;
     public string nombreJugador = "Fernando";
 
+    private void Awake() {
+        Debug.Log("Awake: Inicializando propiedades de:" + nombreJugador);
+    }
+
     void Start() {
         Debug.Log("Bienvenido, " + nombreJugador + " al videojuego.");
     }
 
     void Update() {
-        // Debug.Log("Velocidad: " + velocidad + " - Vida = " + vida);
+        //VerificarEstado();
         ControlarEstado();
     }
 
@@ -18,6 +22,7 @@ public class PropiedadesJugador : MonoBehaviour {
         // Evaluar la vida del jugador y mostrar un mensaje si es baja
         if (vida <= 0) {
             Debug.Log("El jugador ha muerto.");
+            vida = 0;
         }
         else if (vida < 20) {
             Debug.Log("Cuidado, tu vida es baja!");
@@ -27,6 +32,12 @@ public class PropiedadesJugador : MonoBehaviour {
         if (velocidad > 10f) {
             Debug.Log("Velocidad máxima alcanzada.");
             velocidad = 10f;
+        }
+    }
+
+    private void VerificarEstado() {
+        if (vida >= 20) {
+            Debug.Log("Velocidad: " + velocidad + " - Vida = " + vida);
         }
     }
 }
