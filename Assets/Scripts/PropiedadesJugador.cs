6e5a175dcdf98f83407255746b137df353219d97
Fernando Flores Quiliche <52175863+fernandobeyond@users.noreply.github.com>
2026-07-08 20:10:47 -0500

    Avances hasta el Sprint 5

--- Assets/Scripts/PropiedadesJugador.cs
+++ Assets/Scripts/PropiedadesJugador.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 public class PropiedadesJugador : MonoBehaviour {
-    [SerializeField] private int vida = 100;
+    [SerializeField] private int vida;
     [SerializeField] private float velocidad = 5.5f;
     public string nombreJugador = "Fernando";
 
@@ -10,11 +10,12 @@ public class PropiedadesJugador : MonoBehaviour {
     }
 
     void Start() {
+        Vida = 2000;
         Debug.Log("Bienvenido, " + nombreJugador + " al videojuego.");
     }
 
     void Update() {
-        //VerificarEstado();
+        VerificarEstado();
         ControlarEstado();
     }
 
@@ -23,8 +24,9 @@ public class PropiedadesJugador : MonoBehaviour {
         if (vida <= 0) {
             Debug.Log("El jugador ha muerto.");
             vida = 0;
-        }
-        else if (vida < 20) {
+        } else if (vida > 100) {
+            vida = 100;
+        } else if (vida < 20) {
             Debug.Log("Cuidado, tu vida es baja!");
         }
 
@@ -38,6 +40,22 @@ public class PropiedadesJugador : MonoBehaviour {
     private void VerificarEstado() {
         if (vida >= 20) {
             Debug.Log("Velocidad: " + velocidad + " - Vida = " + vida);
+        }
+    }
+
+    // Propiedad publica para acceder a la vida del jugador desde otros scripts
+    public int Vida {
+        get { return vida; }
+        set {
+            if (value < 0) {
+                vida = 0;
+            }
+            else if (value > 100) {
+                vida = 100;
+            }
+            else {
+                vida = value;
+            }
         }
     }
 }
